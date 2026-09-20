c8f00327cbda614c1e676e36c61a11e21e447953
Omar Fathi <its.omar.fathy@gmail.com>
2026-09-20 19:14:11 +0300

    feat: practice common array methods

--- AcademyScheduleAnalyzer/AcademyScheduleAnalyzer/Program.cs
+++ AcademyScheduleAnalyzer/AcademyScheduleAnalyzer/Program.cs
@@ -43,10 +43,77 @@ class Program
 
         #region Run  Part 3 — Search for a Session
 
-        Console.Write("Enter a session name: ");
-        string sessionName = Console.ReadLine()!;
+        // Console.Write("Enter a session name: ");
+        // string sessionName = Console.ReadLine()!;
+        //
+        // SearchForASession(sessionName, sessionNames, sessionDates, sessionDurations);
+        #endregion
+
+        #region Run Part 4 — Array Methods Practice
+
+        #region Run 4.1 Sort Session Names
+
+        // Console.WriteLine("4.1 Sort Session Names");
+        // Console.WriteLine($"----------------------");
+        // SortSessionNames(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
+        #region Run 4.2 Reverse Session Names
+
+        // Console.WriteLine("4.2 Reverse Session Names");
+        // Console.WriteLine($"----------------------");
+        // ReverseSessionNames(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
+        #region Run 4.3 Find Session Index
+
+        // Console.WriteLine("4.3 Find Session Index");
+        // Console.WriteLine($"----------------------");
+        // FindSessionIndex(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
+        #region Run 4.4 Check if a Session Exists
+
+        // Console.WriteLine("4.4 Check if a Session Exists");
+        // Console.WriteLine($"----------------------");
+        // CheckIfSessionExists(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
+        #region Run 4.5 Find a Session
         
-        SearchForASession(sessionName, sessionNames, sessionDates, sessionDurations);
+        // Console.WriteLine("4.5 Find a Session");
+        // Console.WriteLine($"----------------------");
+        // FindSession(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
+        #region Run 4.6 Find a Session Index Using a Condition
+
+        // Console.WriteLine("4.6 Find a Session Index Using a Condition");
+        // Console.WriteLine($"----------------------");
+        // FindSessionIndexUsingCondition(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
+        #region Run 4.7 Copy an Array
+
+        // Console.WriteLine("4.7 Copy an Array");
+        // Console.WriteLine($"----------------------");
+        // CopyAnArray(sessionNames);
+        // Console.WriteLine($"----------------------");
+
+        #endregion
+
         #endregion
     }
 
@@ -79,6 +146,110 @@ class Program
         }
         DisplayAllSessions([sessionNames[index]],[sessionDates[index]],[sessionDurations[index]]);
     }
+
+    #endregion
+
+    #region Part 4 — Array Methods Practice
+
+    #region 4.1 Sort Session Names
+
+    static void SortSessionNames(string[] sessionNames)
+    {
+        string[] copyOfSessionNames = new string[sessionNames.Length];
+        Array.Copy(sessionNames, copyOfSessionNames, sessionNames.Length);
+        Array.Sort(copyOfSessionNames);
+        foreach (var item in copyOfSessionNames)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    #endregion
+
+    #region 4.2 Reverse Session Names
+
+    static void ReverseSessionNames(string[] sessionNames)
+    {
+        string[] copyOfSessionNames = new string[sessionNames.Length];
+        Array.Copy(sessionNames, copyOfSessionNames, sessionNames.Length);
+        Array.Reverse(copyOfSessionNames);
+        foreach (var item in copyOfSessionNames)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    #endregion
+
+    #region 4.3 Find Session Index
+
+    static void FindSessionIndex(string[] sessionNames)
+    {
+        Console.Write("Enter session name: ");
+        string sessionName = Console.ReadLine()!;
+        int index = sessionNames.IndexOf(sessionName);
+        if(index == -1)
+            Console.WriteLine($"session not found.");
+        else
+            Console.WriteLine($"Index: {index}");
+
+    }
+
+    #endregion
+
+    #region 4.4 Check if a Session Exists
+
+    static void CheckIfSessionExists(string[] sessionNames)
+    {
+        Console.Write("Enter session name: ");
+        string sessionName = Console.ReadLine()!;
+        bool isExist =Array.Exists(sessionNames, name => name.Equals(sessionName,StringComparison.OrdinalIgnoreCase));
+        if(isExist)
+            Console.WriteLine("Session exists.");
+        else
+            Console.WriteLine("Session does not exist.");
+    }
+
+    #endregion
+
+    #region 4.5 Find a Session
+
+    static void FindSession(string[] sessionNames)
+    {
+        var sessionName =Array.Find(sessionNames, name => name.Length > 5);
+        Console.WriteLine($"Session name: {sessionName}");
+    }
+
+    #endregion
+
+    #region 4.6 Find a Session Index Using a Condition
+
+    static void FindSessionIndexUsingCondition(string[] sessionNames)
+    {
+        var sessionIndex =Array.FindIndex(sessionNames, name => name.Equals("Functions", StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine($"Session Index: {sessionIndex}");
+    }
+
+    #endregion
+
+    #region 4.7 Copy an Array
+
+    static void CopyAnArray(string[] sessionNames)
+    {
+        string[] copyOfSessionNames = new string[sessionNames.Length];
+        Array.Copy(sessionNames , copyOfSessionNames , sessionNames.Length);
+        copyOfSessionNames[0] = "Interfaces";
+        
+        Console.WriteLine("Original array :");
+        foreach (var item in sessionNames)
+            Console.WriteLine(item);
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Copied array :");
+        foreach (var item in copyOfSessionNames)
+            Console.WriteLine(item);
+    }
+
+    #endregion
 
     #endregion
 }
