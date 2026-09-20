06b2685d69b3a419eea0727abc68c11a39611674
Omar Fathi <its.omar.fathy@gmail.com>
2026-09-20 17:24:13 +0300

    feat: add session display function

--- AcademyScheduleAnalyzer/AcademyScheduleAnalyzer/Program.cs
+++ AcademyScheduleAnalyzer/AcademyScheduleAnalyzer/Program.cs
@@ -34,5 +34,28 @@ class Program
         };
 
         #endregion
+
+        #region Run Part 2 — Display All Sessions
+
+        DisplayAllSessions(sessionNames,sessionDates,sessionDurations);
+
+        #endregion
     }
+
+    #region Part 2 — Display All Sessions
+
+    static void DisplayAllSessions(string[] sessionNames , DateTime[] sessionDates , int[] sessionDurations )
+    {
+        int length = sessionNames.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine($"{i+1}. {sessionNames[i]}\n" +
+                              $"Date: {DateOnly.FromDateTime(sessionDates[i]).ToString("dd MMMM yyyy")}\n" +
+                              $"Start Time: {TimeOnly.FromDateTime(sessionDates[i]).ToString("hh:mm tt")}\n" +
+                              $"Duration: {sessionDurations[i]} minutes\n");
+            
+        }
+    }
+
+    #endregion
 }
