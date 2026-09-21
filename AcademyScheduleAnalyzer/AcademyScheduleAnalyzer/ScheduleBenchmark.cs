6747f0e06eb22ad86ac604f4b761df341226a5a3
Omar Fathi <its.omar.fathy@gmail.com>
2026-09-22 00:13:41 +0300

    feat: benchmark string concatenation with different loop sizes

--- AcademyScheduleAnalyzer/AcademyScheduleAnalyzer/ScheduleBenchmark.cs
+++ AcademyScheduleAnalyzer/AcademyScheduleAnalyzer/ScheduleBenchmark.cs
@@ -6,61 +6,32 @@ namespace AcademyScheduleAnalyzer;
 public class ScheduleBenchmark
 {
     
-    private string[] sessionNames =
-    {
-        "C# Basics",
-        "Arrays",
-        "Functions",
-        "Date and Time",
-        "Exception Handling"
-    };
-        
-    private DateTime[] sessionDates =
-    {
-        new DateTime(2026, 9, 10, 18, 0, 0),
-        new DateTime(2026, 9, 13, 18, 0, 0),
-        new DateTime(2026, 9, 17, 18, 0, 0),
-        new DateTime(2026, 9, 20, 18, 0, 0),
-        new DateTime(2026, 9, 24, 18, 0, 0)
-    };
-        
-    private int[] sessionDurations =
-    {
-        180,
-        240,
-        180,
-        240,
-        180
-    };
-
+    [Params(100, 1000, 10000, 100000)]
+    public int Iterations;
+    
     [Benchmark]
     public string StringConcatenation()
     {
-        string allDetails = "";
-        int length = sessionNames.Length;
-        for (int i = 0; i < length; i++)
+        string result = "";
+
+        for (int i = 0; i < Iterations; i++)
         {
-            allDetails +=
-                $"Session Name: {sessionNames[i]}" +
-                $" - Session Date: {sessionDates[i].ToString()}" +
-                $" - Session Duration: {sessionDurations[i].ToString()} minutes \n";
+            result += "Hello";
         }
 
-        return allDetails;
+        return result;
     }
     [Benchmark]
     public string StringBuilderConcatenation()
     {
-        StringBuilder allDetails = new StringBuilder();
-                int length = sessionNames.Length;
-                for (int i = 0; i < length; i++)
-                {
-                    allDetails.Append($"Session Name: {sessionNames[i]}" +
-                                      $" - Session Date: {sessionDates[i].ToString()}" +
-                                      $" - Session Duration: {sessionDurations[i].ToString()} minutes \n");
-                }
-        
-                return allDetails.ToString();
+        string result = "";
+
+        for (int i = 0; i < Iterations; i++)
+        {
+            result += "Hello";
+        }
+
+        return result;
     }
 
 }
